2eff0927f8126a62ae6a4d6488abcb87ac082e71
MyBrown <myaisa.brown03@gmail.com>
2026-06-02 21:58:46 -0400

    new ingredients have their appropriate tags, and everything works

--- TiredGame/Assets/Scripts/OrderUp.cs
+++ TiredGame/Assets/Scripts/OrderUp.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OrderUp : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void OnClick()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("OrderUp");
     }
 }
