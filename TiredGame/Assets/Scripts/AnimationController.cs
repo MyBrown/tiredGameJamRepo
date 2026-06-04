f01a2544607b9f826104396485eb9df7190cf8cf
MyBrown <myaisa.brown03@gmail.com>
2026-06-03 20:36:23 -0400

    Adding a cutscene for the first time in the ingredients scene

---
+++ TiredGame/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class AnimationController : MonoBehaviour
+{
+    //a variable to make sure the animation only plays once at the beginning
+    private float playOnce = 0;
+    private PlayableDirector director;
+    // the object with the timeline in it
+    public GameObject timeline;
+
+    void Awake()
+    {
+        if (playOnce < 1)
+        {
+            director.Play();
+            playOnce = 1;
+        }
+        else
+        {
+            timeline.SetActive(false);
+        }
+    }
+}
